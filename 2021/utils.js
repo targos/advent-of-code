de7a68f1c58e549a358332dd500e0b5d304edaf5
Michaël Zasso <targos@protonmail.com>
2021-12-11 12:15:04 +0100

    add training mode

--- 2021/utils.js
+++ 2021/utils.js
@@ -1,7 +1,11 @@
 import { readFileSync } from 'node:fs';
 
 export function readInput(url, transform) {
-  return readFileSync(new URL('input.txt', url), 'utf-8')
+  return readFileSync(new URL(getInputFile(), url), 'utf-8')
     .split('\n')
     .map((value) => transform(value));
+}
+
+function getInputFile() {
+  return process.argv.includes('train') ? 'train.txt' : 'input.txt';
 }
