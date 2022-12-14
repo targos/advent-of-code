aefdb51f437f2925bcc328b4fe37c84130cd79f0
Michaël Zasso <targos@protonmail.com>
2022-12-14 14:19:36 +0100

    prepare 13-14

---
+++ 2022/14/one.js
@@ -0,0 +1,11 @@
+// https://adventofcode.com/2022/day/14
+
+import { readInput } from '../utils.js';
+
+for await (const line of readInput(import.meta.url, splitInput)) {
+  console.log(line);
+}
+
+function splitInput(input) {
+  return input;
+}
