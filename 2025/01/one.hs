c47ecf47aff4909ec4db380ad192fe9582b20477
Michaël Zasso <targos@protonmail.com>
2025-12-02 20:18:35 +0100

    2025-01.one

--- 2025/01/one.hs
+++ 2025/01/one.hs
@@ -2,4 +2,21 @@ main = do
   input <- getContents
   print (solveExercise input)
 
-solveExercise = lines
+solveExercise = length . filter (== 0) . scanl moveDial start . map parseInstruction . lines
+
+numPositions = 100
+
+start = 50
+
+type Instruction = (Char, Int)
+
+parseInstruction :: String -> Instruction
+parseInstruction (side : rest) = (side, read rest :: Int)
+
+moveDial :: Int -> Instruction -> Int
+moveDial position instruction
+  | side == 'R' = (position + steps) `mod` numPositions
+  | side == 'L' = moveDial position ('R', numPositions - steps)
+  where
+    side = fst instruction
+    steps = snd instruction
