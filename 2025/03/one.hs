f6ae5426dceaf18beae42ff6e3c390853dc56a1c
Michaël Zasso <targos@protonmail.com>
2025-12-05 09:12:25 +0100

    2025-03.one

--- 2025/03/one.hs
+++ 2025/03/one.hs
@@ -2,4 +2,14 @@ main = do
   input <- getContents
   print (solveExercise input)
 
-solveExercise = id
+solveExercise = sum . map findJoltage . lines
+
+findJoltage :: String -> Int
+findJoltage bank = dozen * 10 + unit
+  where
+    digits = map (\d -> read [d]) bank
+    (dozenIndex, dozen) = ifoldl (\i (prevI, prevV) v -> if v > prevV then (i, v) else (prevI, prevV)) (-1, -1) (init digits)
+    unit = maximum (drop (dozenIndex + 1) digits)
+
+ifoldl :: (Int -> r -> a -> r) -> r -> [a] -> r
+ifoldl f r t = foldr (\c k i x -> k (i + 1) (f i x c)) (\_ x -> x) t 0 r
